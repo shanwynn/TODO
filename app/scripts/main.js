e45fbf5a811c2a3081656f7edf01e2bcecd1a6d6
Shannon Wynn <shannonwynn@mac.com>
2014-10-24 16:46:56 -0400

    check mark is functioning.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,28 @@ var todoTemplate   = _.template( $('#todo-template').html() );
 var footerTemplate = _.template( $('#footer-template').html() );
 // Don't remove anything above this line ----------------------- //
 
+$('#new-todo').keydown(function(event) {
+  if (event.keyCode === 13) {
+    $('#new-todo').submit();
+    console.log("Enter has been pressed.");
+  }
+});
 
+$('#todo-list').append(todoTemplate({
+    id: 1,
+    title: 'Test Item',
+    completed: ' ',
+    checked: ''
+}));
+
+$('[type = checkbox]')
+  .on('click', function(event){
+  $(this).closest("li").toggleClass("completed");
+  $('#todo-list').prop('checked', false);
+  console.log("Check mark is pressed.");
+});
+
+/*
 //These are examples, please remove and replace with your own code
 $('#todo-list').append(todoTemplate({
     id: 1,
@@ -22,3 +43,4 @@ $('#todoapp footer').html(footerTemplate({
     completedTodos: 0,
     completedClass: 'hide'
 }));
+*/
